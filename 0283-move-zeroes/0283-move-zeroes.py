82d4087d3136fa91386dbbf3a1f90653094b4ecc
Tanya Katiyar <78269022+Tanya-Katiyar@users.noreply.github.com>
2023-09-01 01:17:04 +0530

    Time: 138 ms (92.83%), Space: 17.9 MB (46.68%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,9 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n=len(nums)
-        for i in range(n):
-            if nums[i]==0:
-                nums.remove(nums[i])
-                nums.insert(n,0)
+        # not optimised
+        # n=len(nums)
+        # for i in range(n):
+        #     if nums[i]==0:
+        #         nums.remove(nums[i])
+        #         nums.insert(n,0)
         
+        #optimised
+        idx=0
+        for num in nums:
+            if num!=0:
+                nums[idx]=num
+                idx+=1
+        while idx<len(nums):
+            nums[idx]=0
+            idx+=1
